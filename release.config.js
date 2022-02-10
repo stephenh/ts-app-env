6eac2cec270fa9df953841855654dee4144dbc61
Ben Limmer <ben@benlimmer.com>
2022-02-10 15:48:26 -0700

    fix: build before release

--- release.config.js
+++ release.config.js
@@ -3,6 +3,13 @@ module.exports = {
   plugins: [
     ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     ["@semantic-release/release-notes-generator", { preset: "conventionalcommits" }],
+    [
+      "@semantic-release/exec",
+      {
+        // Run the build
+        prepareCmd: "npm run build",
+      },
+    ],
     "@semantic-release/npm",
     "@semantic-release/github",
     "@semantic-release/git",
