97963dd2574b39d05c2098040b1e2392d34fca8e
Ben Limmer <ben@benlimmer.com>
2022-02-09 17:53:52 -0700

    run in ci

--- release.config.js
+++ release.config.js
@@ -1,18 +1,7 @@
 module.exports = {
   branches: ["main"],
   plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "conventionalcommits",
-
-        releaseRules: [
-          // catch-alls, in case commit messages do not follow conventional-commits
-          { subject: "*", release: "minor" },
-          { subject: null, release: "minor" },
-        ],
-      },
-    ],
+    ["@semantic-release/commit-analyzer", { preset: "conventionalcommits" }],
     ["@semantic-release/release-notes-generator", { preset: "conventionalcommits" }],
     "@semantic-release/npm",
     "@semantic-release/github",
